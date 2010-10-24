ced37b746f3940692e40074e56f36da9ea5e6c8b
Alex <readmymsg123@gmail.com>
2010-10-24 14:40:57 -0400

    created portal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '13441d654687669ebb928f886ec4e1479f6b922416be940dff6a73deba17fc3b1b8a0b83f1a64fd72283f23b09d74cc704b371d87f11cface32ff191f6eeeeb1'
